1bab7c72ed3a30f83f5ef04274a248a174acb597
Secret198 <coconutisthebest3@gmail.com>
2025-02-19 20:06:36 +0100

    unnecessary parameters

--- vertexShader.glsl
+++ vertexShader.glsl
@@ -31,7 +31,7 @@ float GetPhase(float waveLength)
     return (deltaTime + rand(vec2(waveLength, 1.0))) * (2 / waveLength);
 };
 
-float GetWave(vec2 position, float t)
+float GetWave(vec2 position)
 {
     float amplitude = rand(position);
     float waveLength = rand(position);
@@ -39,15 +39,15 @@ float GetWave(vec2 position, float t)
     float frequency = GetFrequency(waveLength);
     float phase = GetPhase(waveLength);
 
-    return amplitude * sin(dot(directionVector, position) * frequency + t * phase);
+    return amplitude * sin(dot(directionVector, position) * frequency + time * phase);
 };
 
-float AddedWaves(vec2 position, float t)
+float AddedWaves(vec2 position)
 {
     float waves = 0;
     for(int i = 0; i < numberOfWaves; i++)
     {
-        waves += GetWave(position, t);
+        waves += GetWave(position);
     }
     return waves;
 };
@@ -55,7 +55,7 @@ float AddedWaves(vec2 position, float t)
 void main()
 {
     vec4 vertexPosition = model * vec4(aPos, 1.0);
-    vertexPosition.y = AddedWaves(vec2(vertexPosition.x, vertexPosition.z), time);
+    vertexPosition.y = AddedWaves(vec2(vertexPosition.x, vertexPosition.z));
     // vertexPosition.y = 2.0 * sin((dot(vec2(0.1, 0.5), vec2(vertexPosition.x, vertexPosition.z)) * (2.0 / 1.0) + time * ((deltaTime) * (2.0 / 1.0)))) + 0.5 * sin((dot(vec2(0.3, 0.5), vec2(vertexPosition.x, vertexPosition.z)) * (2.0 / 2.0) + time * ((deltaTime + 1) * (2.0 / 2.0))));
     gl_Position = projection * view * vertexPosition;
     FragPos = vec3(vertexPosition);
